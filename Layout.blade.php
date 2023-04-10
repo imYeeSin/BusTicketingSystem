505748a9b33550bbd1d9c986680e99f14e42b0eb
Cho Yee Sin <yeesincho7@gmail.com>
2023-04-10 12:53:51 +0800

    Updated Layout

--- Layout.blade.php
+++ Layout.blade.php
@@ -1,22 +1,33 @@
+
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Bus Ticketing System</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </head>
 
     <style>
+
+
         #myNavbar{
             font-size: 17pt;
-            padding-top: 0px;
+            padding: 10px 10px 10px 10px;
+            position: fixed;
+            top: 0;
+            left: 0px;
+            right: 0px;
+            z-index: 9999;
+            background-color: white;
         }
 
         #myNavbar li a{
             color: black;
         }
 
-        #navlogin{
+        #navlogout{
             position: absolute;
             display: inline-block;
             right: 0%;
@@ -43,13 +54,10 @@
                             <a class="nav-link" aria-current="page" href="{{url('/HomePage/home')}}">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="busSchedule.php">Bus Schedule</a>
+                            <a class="nav-link" href="{{url('schedules')}}">Bus Schedule</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="">Bus</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Ticket</a>
+                            <a class="nav-link" href="{{url('buses')}}">Bus</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('Reservation')}}">Reservation</a>
@@ -57,17 +65,19 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('Administrative')}}">Administrator</a>
                         </li>
+
                         <li class="nav-item">
-                            <a class="nav-link" href="">Report</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">Driver</a>
+                            <a class="nav-link" href="{{url('drivers')}}">Driver</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="">Passenger</a>
                         </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Report</a>
+                        </li>
                         <li class="nav-item" >
-                            <a class="nav-link" id ="navlogin"  href="">Login</a>
+                            <a class="nav-link" id ="navlogout"  href="">Logout</a>
                         </li>
                     </ul>
 
@@ -75,9 +85,16 @@
 
         </nav>
 
+        @yield('styles')
+
+        
         <div class="container">
             @yield('content')
+            
         </div>
+
+
+
 
     </body>
 </html>
